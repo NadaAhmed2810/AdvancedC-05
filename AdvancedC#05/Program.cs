3a488671871ef5c2260489a456fc489484ad715f
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-02-01 17:41:22 +0200

    Operation in HashTable

--- AdvancedC#05/Program.cs
+++ AdvancedC#05/Program.cs
@@ -7,22 +7,54 @@ namespace AdvancedC_05
         static void Main(string[] args)
         {
             #region Non_Generic_Collection"HashSet"
-            //Hashtable Note = new Hashtable()
-            //{
-            //    ["Nada"] = 111,
-            //    ["Noura"] = 222,
-            //    ["Nour"]=333,
-            //};
+            Hashtable Note = new Hashtable()
+            {
+                ["Nada"] = 111,
+                ["Noura"] = 222,
+                ["Nour"] = 333,
+            };
+            #region Set / Add / Get 
+            ///Set 
+            /// Note["Ahmed"] = 555;//will update if Ahmed exist and will add if not exist
 
-            //foreach (string key in Note.Keys)
-            //{
-            //    Console.WriteLine(key);
-            //}
-            //foreach (int Value in Note.Values)
-            //{
-            //    Console.WriteLine(Value);
-            //}
-           
+            ///Add 
+            ///unSafe Code 
+            ///Note.Add("Nada", 555);//will Throw Exception
+            ///if (!Note.Contains("Nada"))
+            ///{
+            ///    Note.Add("Nada",555);
+            ///}
+
+            ///Get
+            /// Console.WriteLine(Note["Nada"});//111
+            /// Console.WriteLine(Note["Ahmed]);//will return Null [Not Throw Exception] 
+            #endregion
+
+            #region Looping for HashTable
+            ///UnValid
+            ///for (int i = 0; i < Note.Count; i++)
+            ///{
+            ///    Console.WriteLine(Note[i]);
+            ///}
+
+            ///foreach (DictionaryEntry entry in Note)
+            ///{
+            ///    Console.WriteLine($"Name:{entry.Key}:::Number:{entry.Value}");
+            ///}
+
+            ///foreach (string key in Note.Keys)
+            ///{
+            ///    Console.WriteLine($"Name:{key}");
+            ///}
+
+            ///foreach (int Value in Note.Values)
+            ///{
+            ///    Console.WriteLine($"Number:{Value}");
+            ///} 
+            #endregion
+
+
+
             #endregion
         }
     }
