2ae9f52c9c03423c010fce48ce29cbdd3bf466b4
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-02-01 16:54:38 +0200

    Non_Generic_Collection"HashSet"

--- AdvancedC#05/Program.cs
+++ AdvancedC#05/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace AdvancedC_05
+﻿using System.Collections;
+
+namespace AdvancedC_05
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Non_Generic_Collection"HashSet"
+            //Hashtable Note = new Hashtable()
+            //{
+            //    ["Nada"] = 111,
+            //    ["Noura"] = 222,
+            //    ["Nour"]=333,
+            //};
+
+            //foreach (string key in Note.Keys)
+            //{
+            //    Console.WriteLine(key);
+            //}
+            //foreach (int Value in Note.Values)
+            //{
+            //    Console.WriteLine(Value);
+            //}
+           
+            #endregion
         }
     }
 }
