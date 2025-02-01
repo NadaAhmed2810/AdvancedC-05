c43037eb86dc1a4243f19fbe1617ac3a7645c474
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-02-01 20:23:37 +0200

    Generic Collection "Dictionary"

---
+++ AdvancedC#05/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedC_05
+{
+    internal class Employee:IEquatable<Employee>
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Salary { get; set; }
+       public  Employee() { }
+        public Employee(int Id,string? Name,decimal Salary)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Salary = Salary;
+        }
+
+        public override string ToString()
+        {
+            return $"Id :{Id},Name: {Name},Salary: {Salary}";
+        }
+
+        public bool Equals(Employee? other)
+        {
+            return Id.Equals(other?.Id??0) &&( Name?.Equals(other?.Name??" ")??(other?.Name is null ?true:false)) && Salary.Equals(other?.Salary??0);
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id,Salary,Name);
+        }
+    }
+}
--- AdvancedC#05/Program.cs
+++ AdvancedC#05/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
 
 namespace AdvancedC_05
 {
@@ -22,23 +23,36 @@ namespace AdvancedC_05
         }
 
     }
-    class StringEqualityComparer : IEqualityComparer
+    class StringEqualityComparer : IEqualityComparer<string>
     {
-        public new bool Equals(object? x, object? y)
+       
+
+        public bool Equals(string? x, string? y)
         {
-            string? Strx=x as string;
-            string? Stry=y as string;
-            return Strx?.ToLower().Equals(Stry?.ToLower())??(Stry is null?true:false);
+           
+            return x?.ToLower().Equals(y?.ToLower()) ?? (y is null ? true : false);
         }
 
-        public int GetHashCode(object obj)
+    
+        public int GetHashCode([DisallowNull] string obj)
         {
-            string? s = obj as string;
-            if (s == null)
-                throw new ArgumentNullException(nameof(obj));
-            return s.ToLower().GetHashCode() ;
+       
+            return obj.ToLower().GetHashCode();
         }
     }
+    class EmployeeComparer : IEqualityComparer<Employee>
+    {
+        public bool Equals(Employee? x, Employee? y)
+        {
+            return x?.Id.Equals(y?.Id??0)?? (y is null ? true : false) ;
+        }
+
+        public int GetHashCode([DisallowNull] Employee obj)
+        {
+           return obj.Id.GetHashCode();
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
@@ -98,6 +112,84 @@ namespace AdvancedC_05
 
 
 
+            #endregion
+            #region Generic Collection "Dictionary"
+            #region Constructor
+            //Dictionary<string, int> Note = new Dictionary<string, int>()
+            //{
+            //  ["Ahmed"] =111,
+            //  ["Nadia"]=222,
+            //  ["Nour"]=333, 
+            //};
+            //Dictionary<string, int> Note02 = new Dictionary<string, int>(Note,new StringEqualityComparer());
+            //KeyValuePair<string, int>[] keyValuePairs = new KeyValuePair<string, int>[3]
+            //{
+            //    new KeyValuePair<string, int>("Ahmed",111),
+            //    new KeyValuePair<string, int>("Nada",222),
+            //    new KeyValuePair<string, int>("Nour",333)
+            //};
+            //Dictionary<string, int> Note03 = new Dictionary<string, int>(keyValuePairs);
+
+            #endregion
+
+            #region Add And Set and get 
+            ///ADD
+            ///Note.Add("Girgis", 888);//not safe 
+            ///if (!Note.ContainsKey("Girgis"))
+            ///    Note.Add("Girgis", 888);
+            ///Note.TryAdd("Girgis", 888);//will return bool 
+
+            ///Get 
+            ///Console.WriteLine($"{Note["Ahmed"]}");
+            ///un safe Code
+            ///Console.WriteLine($"{Note["Nada"]}");//will Throw Exception 
+            ///Note.TryGetValue("Nada", out int Value); //return bool
+            ///Console.WriteLine(Value);//return default if not exists 
+
+            ///Set 
+            ///Note["Yasmine"] = 444; //Toggle
+
+            #endregion
+
+            #region Example01 "Key must be Immutable"
+            //Employee employee01 = new Employee(10, "Nada", 10000);
+            //Employee employee02 = new Employee(20, "Nour", 20000);
+            //Employee employee03 = new Employee(30, "Noura", 30000);
+            //Dictionary<Employee, string> employees = new Dictionary<Employee, string>(new EmployeeComparer())
+            //{
+            //    [employee01]=employee01.ToString(),
+            //    [employee02]=employee02.ToString(),
+            //    [employee03]=employee03.ToString(),
+            //};
+
+            //employee01.Id = 90;
+            //employees.Add(new Employee() { Id = 90 }, string.Empty);
+            //employees.Add(new Employee(40, "Radwa", 40000), "Id :40,Name: Radwa,Salary: 40000");
+            //foreach (var employee in employees)
+            //{
+            //    Console.WriteLine($"Employee: {employee.Key}");
+            //} 
+            //employees.Add(new Employee(10, "Nada", 10000), "Nada");
+            //employees.Add(new Employee(){Id =20}," ");
+            #endregion
+
+
+            #region Looping 
+            //foreach (var Person in Note)
+            //{
+            //    Console.WriteLine($"Name:{Person.Key}:::Number:{Person.Value}");
+            //}
+            //Console.WriteLine();
+            //foreach (var key in Note.Keys)
+            //{
+            //    Console.WriteLine(key);
+            //}
+            //Console.WriteLine();
+            //foreach (var value in Note.Values)
+            //{
+            //    Console.WriteLine(value);
+            //} 
+            #endregion
             #endregion
         }
     }
