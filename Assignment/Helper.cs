79d81b19eeda08bb92c0f722e1a969e2dd2dd7dd
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-02-02 13:23:19 +0200

    Q1:

---
+++ Assignment/Helper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Helper
+    {
+        public static void Reverse(ArrayList Numbers)
+        {
+            if (Numbers == null || Numbers.Count == 0) return;
+            int sz=Numbers.Count;
+            for (int i = 0; i < Numbers.Count / 2; i++)
+            {
+                object Temp = Numbers[i];
+                Numbers[i] = Numbers[sz-i-1];
+                Numbers[sz - i - 1] = Temp;
+            }
+
+        }
+        public static void Swap<T>(ref T num1, ref T num2)
+        {
+            T Temp = num1;
+            num1 = num2;
+            num2 = Temp;
+        }
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+namespace Assignment
+{
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            #region Q1:You are given an ArrayList containing a sequence of elements. try to reverse the order of elements in the ArrayList in-place(in the same arrayList) without using the built-in Reverse. Implement a function that takes the ArrayList as input and modifies it to have the reversed order of elements.
+            //ArrayList Numbers = new ArrayList () { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //Helper.Reverse(Numbers);
+            //for (int i = 0; i < Numbers.Count; i++)
+            //{
+            //    Console.WriteLine(Numbers[i]);
+            //}
+            //ArrayList Names = new ArrayList() {"Nada","Nour" ,"Noura","Nadeen"};
+            //Helper.Reverse(Names);
+            //for (int i = 0;i < Names.Count; i++)
+            //{
+            //    Console.WriteLine(Names[i]);
+            //}
+
+            #endregion
+        }
+    }
+}
