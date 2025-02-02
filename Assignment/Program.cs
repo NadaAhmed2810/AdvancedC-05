2997a1aa87ac42b0bc90f4b619d3a08b0265db96
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-02-02 13:39:20 +0200

    Q3

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,6 @@ namespace Assignment
 {
     internal class Program
     {
-      
         static void Main(string[] args)
         {
             #region Q1:You are given an ArrayList containing a sequence of elements. try to reverse the order of elements in the ArrayList in-place(in the same arrayList) without using the built-in Reverse. Implement a function that takes the ArrayList as input and modifies it to have the reversed order of elements.
@@ -27,6 +26,36 @@ namespace Assignment
             //List<int> EvenNumbers=Helper.GetEvenNumbers(Numbers);
             //for(int i=0;i<EvenNumbers.Count;i++) 
             //    Console.WriteLine(EvenNumbers[i]);
+            #endregion
+            #region Q3:FixedSizeList<T> 
+            //FixedSizeList<double> FixedSizeList = new FixedSizeList<double>(10);
+            //double x = 1;
+            //for (int i = 0; i < 11; i++)
+            //{
+            //    try
+            //    {
+            //        FixedSizeList.Add(x += .5);
+            //    }
+            //    catch
+            //    {
+            //        Console.WriteLine("List Is Full");
+            //    }
+
+            //}
+            //try
+            //{
+            //    FixedSizeList.Get(0);
+            //    FixedSizeList.Get(1);
+            //    FixedSizeList.Get(10);
+            //    FixedSizeList.Get(-5);
+            //}
+            //catch
+            //{
+            //    Console.WriteLine("Invalid index");
+            //}
+
+
+
             #endregion
         }
     }
